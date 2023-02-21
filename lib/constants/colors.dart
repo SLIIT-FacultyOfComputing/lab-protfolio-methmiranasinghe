ddd15d485100bc58efe6662de1cf03716315ba9a
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2023-02-21 10:17:23 +0530

    deleted

--- lib/constants/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color todoRed = Color(0xFFDA4040);
-const Color todoBlue = Color(0xFF5f52EE);
-
-const Color todoBlack = Color(0xFF3A3A3A);
-const Color todoGrey = Color(0xFF717171);
-
-const Color todoBgColor = Color(0xFFEEEEF5);
